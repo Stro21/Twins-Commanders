dae9d8ed5b0da98565716c27e55547fb4789a7c1
Eduardo Straub <eduardo.straub1@gmail.com>
2015-10-06 01:12:39 -0300

    Se agrego Squad

---
+++ src/Matrix/SquareData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Matrix;
+
+import Characters.*;
+
+/**
+ *
+ * @author eduar
+ */
+public class SquareData {
+    private BaseCharacter character;
+    private double space_ocupied;    
+}
